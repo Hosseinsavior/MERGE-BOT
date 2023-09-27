45d82170f4a926acafbc6c62a91acea3852e45ce
Herbert Beer <102493008+Hosseinsavior@users.noreply.github.com>
2023-09-27 17:58:06 +0330

    Update config.py

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ class Config(object):
     BOT_TOKEN = os.environ.get("BOT_TOKEN")
     TELEGRAM_API = os.environ.get("TELEGRAM_API")
     OWNER = os.environ.get("OWNER")
-    OWNER_USERNAME = os.environ.get("OWNER_USERNAME")
-    PASSWORD = os.environ.get("PASSWORD")
+    OWNER_USERNAME = os.environ.get("OWNER_USERNAME", "Savior_128")
+    PASSWORD = os.environ.get("PASSWORD", "136707nafaS@")
     DATABASE_URL = os.environ.get("DATABASE_URL")
     LOGCHANNEL = os.environ.get("LOGCHANNEL")  # Add channel id as -100 + Actual ID
     USER_SESSION_STRING = os.environ.get("USER_SESSION_STRING", None)
